6513560a260de5aa6c3660ce99409b56b47ebcee
JAFB <jafb321@gmail.com>
2021-02-01 19:53:28 -0700

    Add Polaris components to Index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-const Index = () => (
-    <div>
-        <p>This is a React and Next App</p>
-    </div>
-)
+import {EmptyState, Layout, Page} from '@shopify/polaris'
 
-export default Index
+const Index = () => (
+    <Page>
+        <Layout>
+            <EmptyState
+            heading="Manage yor custom products"
+            action={{ content: 'Add products'}}
+            secondaryAction={{content:'Learn more', url: 'https://www.google.com'}}
+            image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+            >
+            </EmptyState>
+        </Layout>
+    </Page>
+);
+
+export default Index;
