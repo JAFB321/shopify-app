cda4f8830b13b7ba6ae71bc18c41d522c781c076
JAFB <jafb321@gmail.com>
2021-02-24 19:09:25 -0700

    Add ProductList component content

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,6 +1,8 @@
 import gql from 'graphql-tag';
-import { useQuery } from '@apollo/client';
-import {Card, ResourceList, Stack, TextStyle, Thumbnail} from '@shopify/polaris';
+import { useQuery } from '@apollo/react-hooks';
+import { Card, ResourceList, Stack, TextStyle, Thumbnail } from '@shopify/polaris';
+import { storeKeyNameFromField } from '@apollo/client/utilities';
+import store from 'store-js'
 
 const GET_PRODUCTS_BY_ID = gql`
 query getProductByIds($ids: [ID!]!){
@@ -32,9 +34,68 @@ query getProductByIds($ids: [ID!]!){
 
 export const ProductList = () => {
 
+  const { loading, error, data } = useQuery(GET_PRODUCTS_BY_ID, {
+    variables: {
+      ids: store.get('ids')
+    }
+  });
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error...</div>
+
+  const { nodes } = data;
+
   return (
     <>
-        
+      <Card>
+        <ResourceList
+          showHeader
+          resourceName={{ singular: 'Product', plural: 'Products' }}
+          items={nodes}
+          renderItem={(item) => {
+
+            const featuredImage = item.images.edges[0];
+            const firstVariant = item.variants.edges[0];
+            const media = (
+              <Thumbnail
+                source={
+                  featuredImage ? featuredImage.node.originalSrc : ''
+                }
+                alt={
+                  featuredImage ? featuredImage.node.altText : ''
+                }
+              />
+            );
+
+            const price = firstVariant ? firstVariant.node.price : 0;
+
+            return (
+              <ResourceList.Item
+                id={item.id}
+                media={media}
+                accessibilityLabel={`View details for ${item.title}`}
+              >
+                <Stack alignment="center">
+                  <Stack.Item fill >
+                    <h3>
+                      <TextStyle variation="strong">
+                        {item.title}
+                      </TextStyle>
+                    </h3>
+                  </Stack.Item>
+                  <Stack.Item >
+                    <p>${price}</p>
+                  </Stack.Item>
+                </Stack>
+              </ResourceList.Item>
+            );
+          }}
+        />
+      </Card>
+
+      {/* {
+          nodes.map((product) => <p key={product.id}>{product.title}</p>)
+        } */}
     </>
   )
 }
