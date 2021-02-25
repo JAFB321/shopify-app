44ad0ebfe46650b7f9771f748a5ea5ee02518360
JAFB <jafb321@gmail.com>
2021-02-24 23:28:05 -0700

    use Query and mutation hooks for scriptags

--- pages/ScriptPage.js
+++ pages/ScriptPage.js
@@ -1,4 +1,48 @@
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+const CREATE_SCRIPT_TAG = gql`
+mutation AddScriptTag($Script: ScriptTagInput!){
+    scriptTagCreate(input: $Script){
+      scriptTag{
+        id
+        src
+        createdAt
+        displayScope
+      }
+      userErrors{
+        field
+        message
+      }
+    }
+  }
+`;
+
+const GET_SCRIPT_TAGS = gql`
+query getScriptTags {
+    scriptTags(first:10) {
+      edges {
+        node {
+          id
+          src
+        }
+      }
+    }
+  }
+`;
+
+const SCRIPT = 'https://jafb321testing.loca.lt/test-script.js';
+
 const ScriptPage = () => {
+
+    const { data, error, loading } = useQuery(GET_SCRIPT_TAGS);
+    const [addScript, { data: addScriptData }] = useMutation(CREATE_SCRIPT_TAG);
+
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Error...</div>
+
+    console.log(data);
+
     return (
         <div>
             <h1>Hello this is the Script Page</h1>
